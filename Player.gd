9002b87ffd5607124386b54582c29c994fd90ff2
OliveADay <sharinggon23@gmail.com>
2024-03-01 08:25:09 -0800

    s

--- Player.gd
+++ Player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
+var recentlyFloored = false
 
+var previousFloored = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -29,5 +31,6 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	previousFloored = is_on_floor()
 	move_and_slide()
