72feb249b0d1c88debcd1507ac0af0daab3e3b1e
OliveADay <sharinggon23@gmail.com>
2024-03-13 22:17:14 -0700

    s

--- Player.gd
+++ Player.gd
@@ -9,6 +9,8 @@ var gravM = true
 
 @export var jumpTrue = true
 
+var walkCyc = 0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -19,31 +21,31 @@ func _physics_process(delta):
 		jumpTrue = false
 		var bodies = $GravJumpActiv.get_overlapping_bodies()
 		bodies[0].queue_free()
-		controler.visible = true	
-		#$Control/Timer.start(2)	
+		controler.visible = true
+		$pickupAud.playing = true
 	if $SpikeSense.has_overlapping_bodies():
+		$deadAud.playing = true
 		get_tree().reload_current_scene()
 	
-	if not is_on_floor():
-		
+	if not is_on_floor():	
 		if gravM:
 			velocity.y += gravity * delta
 			$CPUParticles2D.position.y = 8
 		else:
 			velocity.y -= gravity * delta
 			$CPUParticles2D.position.y = -8
-	if $GroundCheck.has_overlapping_bodies():
-		$CPUParticles2D.visible = true
-	else:
-		$CPUParticles2D.visible = false
 		
 			
 	if Input.is_action_just_pressed("shift_up") and not jumpTrue:
+		if gravM:
+			$shiftAud.playing = true
 		gravM = false
 		$Sprite2D.frame = 1
 		$Sprite2D.scale.y = -1
 		velocity.y  = 0
 	if Input.is_action_just_pressed("shift_down") and not jumpTrue:
+		if not gravM:
+			$shiftAud.playing = true
 		gravM = true
 		$Sprite2D.scale.y = 1
 		$Sprite2D.frame = 0
@@ -51,6 +53,8 @@ func _physics_process(delta):
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("shift_up") and is_on_floor():
+			if jumpTrue:
+				$JumpAud.playing = true
 			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -68,5 +72,15 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		#$CPUParticles2D.visible = false
+	if $GroundCheck.has_overlapping_bodies() and direction != 0:
+		if(walkCyc > 0):
+			walkCyc -= delta
+		else:
+			$walkAud.playing = true
+			walkCyc = 0.3
+		$CPUParticles2D.emitting = true
+	else:
+		$CPUParticles2D.emitting = false
+		walkCyc = 0
 		
 	move_and_slide()
